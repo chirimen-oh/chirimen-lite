093d3dc07fd705eb392429e117c0c5a68bc26be4
Kohei Watanabe <kou029w@gmail.com>
2021-09-10 15:40:50 +0900

    enable i2c

--- stage2/99-enable-serial/00-run.sh
+++ stage2/99-enable-serial/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 echo dtoverlay=dwc2 >> "${ROOTFS_DIR}/boot/config.txt"
-install -m 644 {files,${ROOTFS_DIR}}/etc/modprobe.d/g_acm_ms.conf
-install -m 644 {files,${ROOTFS_DIR}}/etc/modules-load.d/g_acm_ms.conf
+install -m 644 {files,"${ROOTFS_DIR}"}/etc/modprobe.d/g_acm_ms.conf
+install -m 644 {files,"${ROOTFS_DIR}"}/etc/modules-load.d/g_acm_ms.conf
 
 on_chroot << EOF
 systemctl enable getty@ttyGS0.service
