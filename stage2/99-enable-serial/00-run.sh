7adc4f585cf832f5ad83e04569cc6c78e3747043
Kohei Watanabe <nebel@fogtype.com>
2023-08-23 22:59:22 +0900

    disable mass storage change g_acm_ms to g_serial

--- stage2/99-enable-serial/00-run.sh
+++ stage2/99-enable-serial/00-run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 
 echo dtoverlay=dwc2 >> "${ROOTFS_DIR}/boot/config.txt"
-install -m 644 {files,"${ROOTFS_DIR}"}/etc/modprobe.d/g_acm_ms.conf
-install -m 644 {files,"${ROOTFS_DIR}"}/etc/modules-load.d/g_acm_ms.conf
+install -m 644 {files,"${ROOTFS_DIR}"}/etc/modules-load.d/g_serial.conf
 
 on_chroot << EOF
 systemctl enable getty@ttyGS0.service
