07ab194eb9ab2659b3a13f7ab439fa5fbe35f0f1
Kohei Watanabe <nebel@fogtype.com>
2025-05-22 13:54:18 +0900

    Switch Node.js installation from n to NodeSource repository
    Replace manual n version manager installation with official NodeSource LTS
    repository setup for more reliable ARM package management and updates.

--- stage2/99-install-nodejs/00-run.sh
+++ stage2/99-install-nodejs/00-run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -e
 
-install -v -m 644 {files,"${ROOTFS_DIR}"}/etc/profile.d/n.sh
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/myApp"
 install -v -o 1000 -g 1000 -m 644 {files,"${ROOTFS_DIR}/home/${FIRST_USER_NAME}"}/myApp/package.json
 
 mount --bind /etc/ssl/certs "${ROOTFS_DIR}/etc/ssl/certs"
 
 on_chroot << EOF
-install <(curl -sL https://raw.githubusercontent.com/tj/n/master/bin/n) /usr/local/bin/n
-. /etc/profile.d/n.sh
-n --arch armv6l lts
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+apt-get -o Acquire::Retries=3 install -y nodejs
 
 sudo -u#1000 npm config set prefix '/home/${FIRST_USER_NAME}/.local'
 sudo -u#1000 npm install -g forever jiti
--- stage2/99-install-nodejs/files/etc/profile.d/n.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-export N_NODE_MIRROR=https://unofficial-builds.nodejs.org/download/release
