56cb53c605eb834edcf129283c0864606db59f1f
Kohei Watanabe <nebel@fogtype.com>
2026-06-26 14:49:41 +0900

    Pin Node.js to latest v22.x unofficial armv6l build

--- stage2/99-install-nodejs/00-run.sh
+++ stage2/99-install-nodejs/00-run.sh
@@ -9,7 +9,7 @@ mount --bind /etc/ssl/certs "${ROOTFS_DIR}/etc/ssl/certs"
 on_chroot << EOF
 install <(curl -sL https://raw.githubusercontent.com/tj/n/master/bin/n) /usr/local/bin/n
 . /etc/profile.d/n.sh
-n --arch armv6l lts
+n --arch armv6l 22
 
 npm install -g forever
 sudo -u#1000 npm --prefix '/home/${FIRST_USER_NAME}/myApp' install
